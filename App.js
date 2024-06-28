253665fb966d60913ed2270c574b2157e7dd2a1d
Tanner Davison <tanner.davison95@gmail.com>
2024-06-27 23:24:51 -0600

    updated some ui styles

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
-import { StyleSheet, View, FlatList, Button } from "react-native";
+import { StyleSheet, View, FlatList, Text, Pressable } from "react-native";
 
 export default function App() {
   const [allGoals, setAllGoals] = useState([]);
@@ -30,37 +30,48 @@ export default function App() {
 
   return (
     <>
-    <StatusBar style='light'/>
-    <View style={styles.appContainer}>
-      <Button title={"Add New Goal"} color="#a065ec" onPress={modalHandler} />
-
-      <GoalInput
-        onAddGoal={addGoalHandler}
-        showmodal={modalVisible}
-        onCancel={goalEndHandler}
-      />
-
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={allGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                id={itemData.item.id}
-                text={itemData.item.text}
-                onDelete={deleteGoalHandler}
-              />
-            );
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Pressable
+          style={({ pressed }) => {
+            return pressed
+              ? styles.pressedGoalInput // Combine styles for the pressed state
+              : styles.addGoalBtn;
           }}
+          underlayColor="#0056b3"
+          onPress={modalHandler}
+        >
+          <Text style={styles.buttonText}> Add New Goal</Text>
+        </Pressable>
+
+        <GoalInput
+          onAddGoal={addGoalHandler}
+          showmodal={modalVisible}
+          onCancel={goalEndHandler}
         />
+
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={allGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  id={itemData.item.id}
+                  text={itemData.item.text}
+                  onDelete={deleteGoalHandler}
+                />
+              );
+            }}
+          />
+        </View>
       </View>
-    </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
+    position: "relative",
     paddingHorizontal: 16,
     paddingTop: 45,
     flex: 1,
@@ -77,5 +88,31 @@ const styles = StyleSheet.create({
 
   goalsContainer: {
     flex: 4,
+  },
+  addGoalBtn: {
+    position: "absolute",
+    bottom: 50,
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 8,
+    borderWidth: 3,
+    padding: 10,
+    backgroundColor: "black",
+    zIndex: 2,
+  },
+  pressedGoalInput: {
+    position: "absolute",
+    bottom: 50,
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 8,
+    borderWidth: 3,
+    padding: 10,
+    backgroundColor: "#001221",
+    zIndex: 2,
+  },
+  buttonText: {
+    color: "white",
+    alignSelf: "center",
   },
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -5,14 +5,24 @@ import {
   Button,
   Modal,
   Image,
+  TouchableHighlight,
+  Pressable,
 } from "react-native";
 import { useState } from "react";
 import Goals from "../assets/GOALS.png";
 const GoalInput = (props) => {
   const [userGoal, setUserGoal] = useState("");
+  const [focused, setFocused] = useState(false);
 
   const handleGoalChange = (enteredText) => {
     setUserGoal(enteredText);
+  };
+  const onFocus = () => {
+    setFocused(true);
+    console.log("focused now");
+  };
+  const onBlur = () => {
+    setFocused(false);
   };
 
   const addGoalHandler = () => {
@@ -24,15 +34,23 @@ const GoalInput = (props) => {
     <Modal visible={props.showmodal} animationType="slide">
       <View style={styles.inputContainer}>
         <Image style={styles.image} source={Goals} width={10} height={10} />
+
         <TextInput
-          style={styles.textInput}
+          style={focused ? styles.focused : styles.textInput}
           placeholder="Your list of goals"
           onChangeText={handleGoalChange}
           value={userGoal}
+          onFocus={() => onFocus()}
+          onBlur={() => onBlur()}
         />
+
         <View style={styles.buttonContainer}>
           <View style={styles.buttons}>
-            <Button title="+ Add Goal" onPress={addGoalHandler} color="#b180f0" />
+            <Button
+              title="+ Add Goal"
+              onPress={addGoalHandler}
+              color="#b180f0"
+            />
           </View>
           <View style={styles.buttons}>
             <Button title="Close" color="#f31282" onPress={props.onCancel} />
@@ -53,18 +71,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     borderBottomWidth: 2,
     flex: 1,
- 
+
     backgroundColor: "black",
   },
   buttonContainer: {
     flexDirection: "row-reverse",
     padding: 10,
-    margin:10,
+    margin: 10,
   },
   buttons: {
     width: 100,
     marginHorizontal: 10,
-   flex:1,
+    flex: 1,
   },
   textInput: {
     borderWidth: 1,
@@ -74,12 +92,24 @@ const styles = StyleSheet.create({
     padding: 16,
     alignItems: "center",
     borderRadius: 8,
-    borderColor:'#e4d0ff',
-    backgroundColor:'#e4d0ff',
-    boderRadius:6,
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    boderRadius: 6,
     color: "#120438",
   },
-
+  focused: {
+    borderWidth: 1,
+    borderColor: "#cccccccc",
+    width: "100%",
+    marginRight: 8,
+    padding: 16,
+    alignItems: "center",
+    borderRadius: 8,
+    borderColor: "purple",
+    backgroundColor: "black",
+    color: "white",
+    boderRadius: 6,
+  },
   image: {
     width: 150,
     height: 150,
