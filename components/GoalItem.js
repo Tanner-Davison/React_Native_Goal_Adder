3fd59ff6e561ee3b2a8925013baa60e47fe24b2e
Tanner Davison <tanner.davison95@gmail.com>
2024-07-11 00:20:13 -0600

    added list

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,13 +1,17 @@
-import { View, Text, StyleSheet, Pressable, } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 const GoalItem = (props) => {
+
   return (
     <View style={styles.goalItem}>
-      <Pressable 
-      onPress={props.onDelete.bind(this, props.id)}
-      style={({pressed})=> pressed && styles.pressedItem || styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </Pressable>
+      <Pressable
+        onPress={props.onDelete.bind(this, props.id)}
+        style={({ pressed }) =>
+          (pressed && styles.pressedItem) || styles.goalItem
+        }
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 };
@@ -16,26 +20,23 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
-    position:'relative',
+    position: "relative",
     margin: 3,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
-    justifyContent:'center',
-    height:50,
+    justifyContent: "center",
+    height: 50,
   },
-  pressedItem:{
-    opacity:1,
-    backgroundColor:"crimson",
-    height:50,
-    justifyContent:'center',
-    borderRadius:6,
-
+  pressedItem: {
+    opacity: 1,
+    backgroundColor: "crimson",
+    height: 50,
+    justifyContent: "center",
+    borderRadius: 6,
   },
   goalText: {
-    position:'relative',
+    position: "relative",
     color: "white",
-    alignSelf:'center',
- 
-    
+    alignSelf: "center",
   },
 });
--- components/NodeRetrieval.js
+++ components/NodeRetrieval.js
@@ -1,36 +1,88 @@
-import {Modal, View, StyleSheet, Button, Text} from 'react-native'
-
+import {
+  Modal,
+  View,
+  StyleSheet,
+  Button,
+  Text,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 const NodeRetrieval = (props) => {
+  const listItems = [
+    "Alligator",
+    "Chupacabra",
+    "Beaver",
+    "Cheatah",
+    "Gorrilla",
+    "Lion",
+    "Wolf",
+    "Mokey",
+    "Zebra",
+  ];
   return (
-    <Modal animationType='slide' visible={props.showApi}>
+    <Modal animationType="slide" visible={props.showApi}>
       <View style={styles.apiContainer}>
         <Text style={styles.headerText}>Hello world</Text>
-        <Button title="Go Back" style={styles.button} onPress={props.apiModalHandler}/>
+       
+          <FlatList
+            horizontal={true}
+            data={listItems}
+            style={styles.listView}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={styles.itemSelf}>
+                  <Text>
+                    {index + 1}. {item}
+                  </Text>
+                </View>
+              );
+            }}
+          />
+        
+        <Button
+          title="Go Back"
+          style={styles.button}
+          onPress={props.apiModalHandler}
+        />
       </View>
     </Modal>
-  )
-}
+  );
+};
 
-export default NodeRetrieval
+export default NodeRetrieval;
 
 const styles = StyleSheet.create({
-    apiContainer:{
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingHorizontal: 16,
-        borderBottomWidth: 2,
-        flex: 1,
+  apiContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingTop:35,
+    borderBottomWidth: 2,
     
-        backgroundColor: "black",
+    flex: 1,
 
-    },
-    headerText:{
-        color:'white',
-    },
-    button: {
-        color: 'blue',
-     
-      },
-})
+    backgroundColor: "black",
+  },
+  listView: {
+    margin: 5,
+  },
+  itemSelf: {
+    backgroundColor: "yellow",
+    color: "black",
+    marginTop: 10,
+    marginRight:10,
+    alignItems:'center',
+    justifyContent:'center',
+    width:100,
+    height:25,
+  },
+  headerText: {
+    color: "white",
+  },
+  button: {
+    color: "blue",
+  },
+});
